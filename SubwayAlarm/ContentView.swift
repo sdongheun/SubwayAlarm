1ccf94d7dfd34f5211359b06fb860a556e872946
sdongheun <sdongheun@gmail.com>
2026-01-02 11:23:35 +0900

    Initial Commit

---
+++ SubwayAlarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SubwayAlarm
+//
+//  Created by 신동흔 on 1/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SubwayAlarm/SubwayAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubwayAlarmApp.swift
+//  SubwayAlarm
+//
+//  Created by 신동흔 on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct SubwayAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
